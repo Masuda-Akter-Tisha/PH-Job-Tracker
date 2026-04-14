05d75943c6b5e522f6842eec10b79fad9f87d3ec
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-14 12:31:22 +0600

    3 btns toggling function has been made

--- script/script.js
+++ script/script.js
@@ -14,3 +14,23 @@ function calculateCount () {
 }
 
 calculateCount ();
+
+// 3 buttons toggling
+const allFilterBtn = document.getElementById ('all-filter-btn');
+const interviewFilterBtn = document.getElementById ('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById ('rejected-filter-btn');
+
+function togglingStyle (id) {
+  allFilterBtn.classList.remove ('text-white', 'bg-[#3B82F6]');  
+  interviewFilterBtn.classList.remove ('text-white', 'bg-[#3B82F6]');  
+  rejectedFilterBtn.classList.remove ('text-white', 'bg-[#3B82F6]'); 
+
+  allFilterBtn.classList.add ('text-[#64748B]', 'bg-white', 'border', 'border-[#F1F2F4]');
+  interviewFilterBtn.classList.add ('text-[#64748B]', 'bg-white', 'border', 'border-[#F1F2F4]');
+  rejectedFilterBtn.classList.add ('text-[#64748B]', 'bg-white', 'border', 'border-[#F1F2F4]');
+
+  const selected = document.getElementById (id);
+  selected.classList.remove ('text-[#64748B]', 'bg-white', 'border', 'border-[#F1F2F4]');
+  selected.classList.add ('text-white', 'bg-[#3B82F6]');
+
+}
